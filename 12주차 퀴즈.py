5db2d98edb29506825f68bfad080c055fd8dd2bd
gull2112 <163505052+gull2112@users.noreply.github.com>
2024-05-24 15:49:33 +0900

    12주차 퀴즈
    붕어빵 퀴즈입니다

--- 12주차 퀴즈.py
+++ 12주차 퀴즈.py
@@ -1,19 +1,23 @@
 class Bungeoppang:
-    def __init__(self, contents):
+    total_sales = 0  # 클래스 변수로 모든 인스턴스에서 공유됨
+
+    def __init__(self, contents, price):
         self.contents = contents
+        self.price = price  
 
     def sell(self):
-        print(f"{self.contents} 붕어빵이 판매되었습니다.")
+        print(f"{self.contents} 붕어빵이 판매되었습니다. 가격은 {self.price}원입니다.")
+        Bungeoppang.total_sales += self.price
+        print(f"총 판매금은 {Bungeoppang.total_sales}원입니다.")
 
     def eat(self):
         print(f"{self.contents} 붕어빵을 먹습니다.")
 
-
 # 슈크림 붕어빵 객체 생성
-cream_bungeoppang = Bungeoppang("슈크림")
+cream_bungeoppang = Bungeoppang("슈크림", 2000)
 
 # 팥 붕어빵 객체 생성
-red_bean_bungeoppang = Bungeoppang("팥")
+red_bean_bungeoppang = Bungeoppang("팥", 1500)
 
 # 판매
 cream_bungeoppang.sell()
